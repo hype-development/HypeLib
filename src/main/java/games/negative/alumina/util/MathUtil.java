54417ad4d68883ea57a476286a0ebe370116439a
Eric <negggative@gmail.com>
2023-10-08 05:58:05 -0300

    Add math utility

---
+++ src/main/java/games/negative/alumina/util/MathUtil.java
@@ -0,0 +1,74 @@
+package games.negative.alumina.util;
+
+/**
+ * A utility class with simple mathematical functions.
+ */
+public class MathUtil {
+
+    /**
+     * This method will check if the given value is between the given min and max.
+     * @param value The value to check.
+     * @param min The minimum value.
+     * @param max The maximum value.
+     * @return True if the value is between the min and max, otherwise false.
+     */
+    public static boolean between(int value, int min, int max) {
+        return value >= min && value <= max;
+    }
+
+    /**
+     * This method will check if the given value is between the given min and max.
+     * @param value The value to check.
+     * @param min The minimum value.
+     * @param max The maximum value.
+     * @return True if the value is between the min and max, otherwise false.
+     */
+    public static boolean between(double value, double min, double max) {
+        return value >= min && value <= max;
+    }
+
+    /**
+     * This method will check if the given value is between the given min and max.
+     * @param value The value to check.
+     * @param min The minimum value.
+     * @param max The maximum value.
+     * @return True if the value is between the min and max, otherwise false.
+     */
+    public static boolean between(float value, float min, float max) {
+        return value >= min && value <= max;
+    }
+
+    /**
+     * This method will check if the given value is between the given min and max.
+     * @param value The value to check.
+     * @param min The minimum value.
+     * @param max The maximum value.
+     * @return True if the value is between the min and max, otherwise false.
+     */
+    public static boolean between(long value, long min, long max) {
+        return value >= min && value <= max;
+    }
+
+    /**
+     * This method will check if the given value is between the given min and max.
+     * @param value The value to check.
+     * @param min The minimum value.
+     * @param max The maximum value.
+     * @return True if the value is between the min and max, otherwise false.
+     */
+    public static boolean between(short value, short min, short max) {
+        return value >= min && value <= max;
+    }
+
+    /**
+     * This method will check if the given value is between the given min and max.
+     * @param value The value to check.
+     * @param min The minimum value.
+     * @param max The maximum value.
+     * @return True if the value is between the min and max, otherwise false.
+     */
+    public static boolean between(byte value, byte min, byte max) {
+        return value >= min && value <= max;
+    }
+
+}
