fcbe0d28af44d86c8a8c5d290317428508c751a6
Eric <negggative@gmail.com>
2023-12-09 16:24:15 -0400

    Add `getByName` in PlayerUtil

--- src/main/java/games/negative/alumina/util/PlayerUtil.java
+++ src/main/java/games/negative/alumina/util/PlayerUtil.java
@@ -2,6 +2,7 @@ package games.negative.alumina.util;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
+import com.google.gson.JsonObject;
 import org.bukkit.GameMode;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeInstance;
@@ -11,12 +12,11 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.potion.PotionEffect;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
-import java.util.function.Consumer;
+import java.util.UUID;
 
 /**
  * Player utility to handle some player-related tasks.
@@ -25,6 +25,7 @@ public class PlayerUtil {
 
     /**
      * Reset the player to default properties.
+     *
      * @param player The player to reset.
      */
     public static void reset(final Player player) {
@@ -58,6 +59,7 @@ public class PlayerUtil {
 
     /**
      * Reset the player's health.
+     *
      * @param player The player to reset.
      */
     private static void resetHealth(final Player player) {
@@ -75,14 +77,17 @@ public class PlayerUtil {
 
     /**
      * Fills the player's inventory with the given collection of items
-     * @param player The player
+     *
+     * @param player     The player
      * @param collection The collection of items
-     * @param failure When the inventory is full of a given item, this consumer is called
-     * @param <T> The type of collection
+     * @param <T>        The type of collection
+     * @return The collection of items that could not be added to the player's inventory
      */
-    private <T extends Iterable<ItemStack>> void fillInventory(final Player player, final T collection, final Consumer<ItemStack> failure) {
+    private <T extends Iterable<ItemStack>> Collection<ItemStack> fillInventory(final Player player, final T collection) {
         Preconditions.checkNotNull(player, "'player' cannot be null!");
         Preconditions.checkNotNull(collection, "'collection' cannot be null!");
+
+        List<ItemStack> failed = Lists.newArrayList();
 
         PlayerInventory inv = player.getInventory();
         for (ItemStack item : collection) {
@@ -114,13 +119,26 @@ public class PlayerUtil {
             if (amount <= 0) continue;
 
             item.setAmount(amount);
-
-            // When the inventory is full of the item, call the failure consumer
-            if (failure == null) continue;
-
-            failure.accept(item);
+            failed.add(item);
         }
 
         player.updateInventory();
+        return failed;
+    }
+
+    /**
+     * Get the player's UUID by their username.
+     * @param username The username of the player.
+     * @return The UUID of the player.
+     * @throws IOException If an error occurs while getting the UUID.
+     */
+    public static UUID getByName(final String username) throws IOException {
+        Preconditions.checkNotNull(username, "'username' cannot be null!");
+
+        JsonObject response = HTTPUtil.get("https://api.mojang.com/users/profiles/minecraft/" + username);
+        if (response == null) return null;
+
+        String uuidStr = response.get("id").getAsString();
+        return UUID.fromString(uuidStr);
     }
 }
