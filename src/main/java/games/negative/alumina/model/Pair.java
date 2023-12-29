48bd2397e1aac663c49397c058880e9a9cd56f8d
Eric <negggative@gmail.com>
2023-12-29 17:28:31 -0400

    Simplify Pair object

---
+++ src/main/java/games/negative/alumina/model/Pair.java
@@ -0,0 +1,11 @@
+package games.negative.alumina.model;
+
+/**
+ * Represents a generic pair of left and right values.
+ *
+ * @param <L> The type of the left value.
+ * @param <R> The type of the right value.
+ */
+public record Pair<L, R>(L left, R right) {
+
+}
